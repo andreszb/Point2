a49c990f935ec48e9a13fe04486e0db8fdcbb8cb
Andres <Andres@DESKTOP-ULEBR6F>
2016-06-23 17:08:17 -0500

    Add project files.

---
+++ Point/Views/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using Point.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using SQLite.Net.Attributes;
+using System.Diagnostics;
+
+namespace Point.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        string path;
+        SQLite.Net.SQLiteConnection conn;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+               "db.sqlite");
+            conn = new SQLite.Net.SQLiteConnection(new
+               SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<Customer>();
+        }
+
+        private void Retrieve_Click(object sender, RoutedEventArgs e)
+        {
+            var query = conn.Table<Customer>();
+            string id = "";
+            string name = "";
+            string age = "";
+
+            foreach (var message in query)
+            {
+                id = id + " " + message.Id;
+                name = name + " " + message.Name;
+                age = age + " " + message.Age;
+            }
+
+            textBlock2.Text = "ID: " + id + "\nName: " + name + "\nAge: " + age;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+            var s = conn.Insert(new Customer()
+            {
+                Name = textBox.Text,
+                Age = textBox1.Text
+            });
+
+        }
+    }
+
+    public class Customer
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+
+}
